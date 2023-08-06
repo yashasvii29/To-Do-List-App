f44a5a7789e0213db57e43c0985b379795cca07e
Yashasvi Agrawal <122177943+yashasvii29@users.noreply.github.com>
2023-08-06 19:14:48 +0530

    Add files via upload

---
+++ To-Do List App/app.js
@@ -0,0 +1,34 @@
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.append(li);
+        let span=document.createElement("span");
+        span.innerHTML= "\u00d7";
+        li.append(span);
+    }
+    inputBox.value="";
+}
+listContainer.addEventListener("click",(e)=>{
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
